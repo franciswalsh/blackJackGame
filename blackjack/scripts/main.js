8568e5cf55a82e968a136597eb21e89fafc0605c
Frank <francisw396@gmail.com>
2017-06-13 10:58:30 -0400

    all the assertions have passed

--- blackjack/scripts/main.js
+++ blackjack/scripts/main.js
@@ -5,15 +5,37 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
+  let totalValue = 0;
+  for (let j in hand){
+    if (hand[j] === 'A'){
+      hand[j] = hand[hand.length-1];
+      hand[hand.length-1] = "A";
+    }
+  }
+  for (let i in hand){
+      if (hand[i] === 'K' || hand[i] === 'Q' || hand[i] === 'J'){
+        totalValue += 10;
+      }
+      else if (hand[i] === 'A'){
+        if (totalValue > 11){
+          totalValue += 1;
+        }
+        else {
+          totalValue += 11;
+        }
+      }
+      else {
+        totalValue += parseInt(hand[i]);
+      }
+    }
 
-
-  return;
+  return totalValue;
 }
 
 
